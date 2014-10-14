6e5310a859e63281e1be795f0a9a83a083a4ee0b
Herbstrith <viniherbstrith@gmail.com>
2014-10-14 01:07:38 -0300

    PajeBinaryReader class mockup

---
+++ src/libpaje/PajeBinaryReader.cc
@@ -0,0 +1,120 @@
+/*
+    This file is part of PajeNG
+
+    PajeNG is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    PajeNG is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Public License for more details.
+
+    You should have received a copy of the GNU Public License
+    along with PajeNG. If not, see <http://www.gnu.org/licenses/>.
+*/
+#include "PajeBinaryReader.h"
+#include "PajeException.h"
+
+PajeBinaryReader::PajeBinaryReader (PajeDefinitions *definitions,)
+{
+  defStatus = OUT_DEF;
+  currentEvent = 0;
+  defs = definitions;
+
+  bzero(&rastro, sizeof(rst_rastro_t));
+  //open rst_file
+   int status = rst_open_file (&rastro, 100000,
+                                arguments.input[i],
+                                arguments.synchronization_file);
+    if (status == RST_NOK){
+      //TODO: throw Exception
+      printf("error at openning of the rst_file");
+      return 1;
+  }
+    
+}
+
+PajeBinaryReader::~PajeBinaryReader ()
+{
+  eventDefinitions.clear ();
+  /* closing the file */
+  rst_close (&rastro);
+}
+
+
+
+
+void PajeBinaryReader::scanDefinitionLine(u_int32_t definitionArray[])
+{
+  int n = 1;
+                                                //PajeEventId,eventid number,line,defs
+  eventBeingDefined = new PajeEventDefinition (event->v_uint32[0], event->v_uint32[0], currentEvent, defs);
+  eventDefinitions[event->v_uint32[n]] = eventBeingDefined;
+
+  for (; n < event->ct.n_uint32; n=n+2) {
+      eventBeingDefined->addField (event->v_uint32[n], event->v_uint32[n+1],n);
+		}
+  
+}
+
+
+PajeTraceEvent *PajeBinaryReader::scanEventLine (rst_event_t event)
+{
+
+  int eventId = -1;
+  PajeEventDefinition *eventDefinition = NULL;
+
+  eventID = event->type;
+
+  eventDefinition = eventDefinitions[eventId];
+  if (eventDefinition == NULL) {
+    throw PajeDecodeException ("Event with id '"+std::string(eventId)+"' has not been defined");
+  }
+
+  return new PajeTraceEvent (eventDefinition, line);
+
+  
+  
+}
+
+//
+void PajeEventDecoder::inputEntity (PajeObject *data)
+{
+
+  rst_event_t rst_event;
+
+
+  int i=0;
+  /* reading all the file */
+  while (rst_decode_event (&rastro, &rst_event)) 
+  {
+
+    if(rst_event->type == PajeHeaderEventId )
+    {
+       
+      PajeBinaryReader::scanDefinitionLine(rst_event->v_uint32);
+     
+	    currentEvent++;
+    }
+
+    //poti event event
+    else
+    {
+      PajeTraceEvent *event = PajeBinaryReader::scanEventLine(&rst_event);
+      if (event != NULL){
+        PajeComponent::outputEntity(event);
+        delete event;
+        currentEvent++;
+      }
+
+    }
+
+    i++;
+  }
+
+
+
+}
+
---
+++ src/libpaje/PajeBinaryReader.h
@@ -0,0 +1,61 @@
+/*
+    This file is part of PajeNG
+
+    PajeNG is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    PajeNG is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Public License for more details.
+
+    You should have received a copy of the GNU Public License
+    along with PajeNG. If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef __PAJEEVENTDECODER_H__
+#define __PAJEEVENTDECODER_H__
+#include <map>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "PajeComponent.h"
+#include "PajeEventDefinition.h"
+#include "PajeTraceEvent.h"
+#include "PajeDefinitions.h"
+#define __STDC_FORMAT_MACROS
+#include <inttypes.h>
+/*
+extern "C"
+{
+  #include <rastro.h>
+
+}
+*/
+
+
+
+class PajeBinaryReader : public PajeComponent {
+private:
+  enum { OUT_DEF, IN_DEF } defStatus;
+  PajeEventDefinition *eventBeingDefined;
+  std::map<std::string,PajeEventDefinition*> eventDefinitions;
+  rst_rastro_t rastro;  
+  
+  void scanDefinitionLine (u_int32_t definitionArray[]);
+  PajeTraceEvent *scanEventLine (rst_event_t event);
+  long long currentEvent;
+  PajeDefinitions *defs;
+
+public:
+  PajeBinaryReader (PajeDefinitions *definitions);
+  ~PajeBinaryReader (void);
+
+  void inputEntity (PajeObject *data);
+};
+#endif
